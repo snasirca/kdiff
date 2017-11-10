aa8fc18ca74dadf643a1b075fafbe00c473bc2a1
Shahriyar Nasir <contact@snasir.ca>
2017-11-09 21:02:01 -0500

    Removes unnecessary build script code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
-import org.gradle.api.plugins.ExtensionAware
-import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
-
 import org.junit.platform.gradle.plugin.FiltersExtension
-import org.junit.platform.gradle.plugin.EnginesExtension
 import org.junit.platform.gradle.plugin.JUnitPlatformExtension
 
 plugins {
@@ -30,9 +26,11 @@ configure<JUnitPlatformExtension> {
 dependencies {
     compile(kotlin("stdlib"))
     testCompile(kotlin("reflect"))
+
     testCompile("org.junit.platform:junit-platform-runner:1.0.1")
     testCompile("org.jetbrains.spek:spek-api:1.1.5")
     testRuntime("org.jetbrains.spek:spek-junit-platform-engine:1.1.5")
+
     testCompile("com.winterbe:expekt:0.2.0")
 }
 
@@ -43,12 +41,6 @@ repositories {
 fun JUnitPlatformExtension.filters(setup: FiltersExtension.() -> Unit) {
     when (this) {
         is ExtensionAware -> extensions.getByType(FiltersExtension::class.java).setup()
-        else -> throw Exception("${this::class} must be an instance of ExtensionAware")
-    }
-}
-fun FiltersExtension.engines(setup: EnginesExtension.() -> Unit) {
-    when (this) {
-        is ExtensionAware -> extensions.getByType(EnginesExtension::class.java).setup()
         else -> throw Exception("${this::class} must be an instance of ExtensionAware")
     }
 }
