ce4be6774ff773f0f0492c328785ab287377fe08
Shahriyar Nasir <contact@snasir.ca>
2017-11-09 23:03:32 -0500

    Renames tracking method

--- src/main/kotlin/ca/snasir/kdiff/Differ.kt
+++ src/main/kotlin/ca/snasir/kdiff/Differ.kt
@@ -9,16 +9,16 @@ class Differ<in T, DiffKey, DiffValue>(
 
     fun diffChanges(oldCollection: Collection<T>, newCollection: Collection<T>): Collection<Change<DiffKey, DiffValue>> {
         oldCollection.forEach {
-            getChange(keyGetter(it)).oldValue = valueGetter(it)
+            getOrStartTrackingChange(keyGetter(it)).oldValue = valueGetter(it)
         }
         newCollection.forEach {
-            getChange(keyGetter(it)).newValue = valueGetter(it)
+            getOrStartTrackingChange(keyGetter(it)).newValue = valueGetter(it)
         }
 
         return changes.values.filter { it.isAChange() }
     }
 
-    private fun getChange(key: DiffKey): Change<DiffKey, DiffValue> {
+    private fun getOrStartTrackingChange(key: DiffKey): Change<DiffKey, DiffValue> {
         return changes.getOrPut(key, { Change(key, oldValue = nullValue, newValue = nullValue) })
     }
 }
