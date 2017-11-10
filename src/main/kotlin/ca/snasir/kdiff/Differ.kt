60090398667d5bf4c7e791fae73bda880cfbd4c9
Shahriyar Nasir <contact@snasir.ca>
2017-11-09 23:05:58 -0500

    Renames the key and value extracting lambdas

--- src/main/kotlin/ca/snasir/kdiff/Differ.kt
+++ src/main/kotlin/ca/snasir/kdiff/Differ.kt
@@ -3,18 +3,18 @@ package ca.snasir.kdiff
 typealias Changes<Key, Value> = Collection<Change<Key, Value>>
 
 class Differ<in T, Key, Value>(
-    private val keyGetter: (it: T) -> Key,
-    private val valueGetter: (it: T) -> Value,
+    private val keyFor: (it: T) -> Key,
+    private val valueFor: (it: T) -> Value,
     private val nullValue: Value
 ) {
     private val changes = HashMap<Key, Change<Key, Value>>()
 
     fun diffChanges(oldCollection: Collection<T>, newCollection: Collection<T>): Changes<Key, Value> {
         oldCollection.forEach {
-            getOrStartTrackingChange(keyGetter(it)).oldValue = valueGetter(it)
+            getOrStartTrackingChange(keyFor(it)).oldValue = valueFor(it)
         }
         newCollection.forEach {
-            getOrStartTrackingChange(keyGetter(it)).newValue = valueGetter(it)
+            getOrStartTrackingChange(keyFor(it)).newValue = valueFor(it)
         }
 
         return changes.values.filter { it.isAChange() }
